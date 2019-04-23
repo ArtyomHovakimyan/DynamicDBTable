0b4e0446a8e4f3dcbc089f2b53da7cbc9bec13c7
User <artyom.hovakimyan01@gmail.com>
2019-04-23 23:19:39 +0400

    add  system.data

---
+++ Mic.Volo.InsertData/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+u
+
+namespace Mic.Volo.InsertData
+{
+    class Program
+    {
+        string connectionString;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
